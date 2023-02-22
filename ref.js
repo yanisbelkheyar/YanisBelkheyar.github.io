0cd0290b65220bc7378b8808f7a31ceb1d11c235
yanisbelkheyar <yanis.belkheyar@gmail.com>
2023-02-22 14:41:06 +0100

    Update ref.js

--- ref.js
+++ ref.js
@@ -1,2 +1,17 @@
-var references = document.getElementById("references.bib");
-references.innerHTML = bibtex.toHTML();
+var bibtexData = `@article{example-article,
+  author = {Doe, John},
+  title = {Example article},
+  journal = {Journal of Examples},
+  volume = {1},
+  number = {1},
+  pages = {1--10},
+  year = {2022}
+}`;
+
+var bibtex = new BibTex();
+bibtex.content = bibtexData;
+
+var html = bibtex.toHTML();
+
+var references = document.getElementById("references");
+references.innerHTML = html;
